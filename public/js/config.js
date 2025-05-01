28e2d70a6304e5e56ddf120357a3143820cc9899
flaypy <souoflay@gmail.com>
2025-04-30 21:28:30 -0300

    1.03 funcionando

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+// js/firebase-config.js
+
+// Inicialização com Firebase compat (v9+ compat)
+const firebaseConfig = {
+    apiKey: "AIzaSyCb9ayVGTHSCPt2a099wlD9IiE0sXWpzIM",
+    authDomain: "pg-no-ponto.firebaseapp.com",
+    projectId: "pg-no-ponto",
+    storageBucket: "pg-no-ponto.firebasestorage.app",
+    messagingSenderId: "959925382750",
+    appId: "1:959925382750:web:887e509b6ba7589d39cecf"
+  };
+  
